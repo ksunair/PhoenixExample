b49364a5e992b09d8e366cdec0ddc7d2d03f03c8
ksuna <ksuna@DESKTOP-SQMOH6T>
2018-12-02 21:18:11 -0600

    added more

--- apps/auction/lib/auction/user.ex
+++ apps/auction/lib/auction/user.ex
@@ -1,4 +1,5 @@
-defmodule Auction.User do  
+defmodule Auction.User do
+    import Ecto.Changeset  
     use Ecto.Schema  
     
     schema "users" do    
@@ -8,4 +9,28 @@ defmodule Auction.User do
         field :hashed_password, :string    
         timestamps()  
     end 
+
+    def changeset(user, params \\ %{}) do    
+        user    
+        |> cast(params, [:username, :email_address])    
+        |> validate_required([:username, :email_address,:hashed_password])     
+        |> validate_length(:username, min: 3)    
+        |> unique_constraint(:username)  
+    end  
+    
+    def changeset_with_password(user, params \\ %{}) do    
+        user    
+        |> cast(params, [:password])    
+        |> validate_required(:password)    
+        |> validate_length(:password, min: 5)       
+        |> hash_password()    
+        |> changeset(params)                                 
+    end  
+    
+    defp hash_password(%Ecto.Changeset{changes: %{password: password}} = changeset) do     
+        changeset    
+        |> put_change(:hashed_password,Auction.Password.hash(password))  
+    end  
+    
+    defp hash_password(changeset), do: changeset 
 end
