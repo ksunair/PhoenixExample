a07978d8e51131aa1ba0d5853ff2bea6ebaf9a6e
ksuna <ksuna@DESKTOP-SQMOH6T>
2018-12-04 17:39:59 -0600

    adding bidding

--- apps/auction_web/lib/auction_web/controllers/user_controller.ex
+++ apps/auction_web/lib/auction_web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule AuctionWeb.UserController do  
-    use AuctionWeb, :controller  
+    use AuctionWeb, :controller 
+    plug :prevent_unauthorized_access when action in [:show] 
     
     def show(conn, %{"id" => id}) do    
         user = Auction.get_user(id)    
@@ -17,4 +18,23 @@ defmodule AuctionWeb.UserController do
             {:error, user} -> render conn, "new.html", user: user    
         end  
     end 
+    
+    defp prevent_unauthorized_access(conn, _opts) do    
+        current_user = Map.get(conn.assigns, :current_user) 
+        
+        requested_user_id =      
+            conn.params      
+            |> Map.get("id")      
+            |> String.to_integer()    
+
+        if current_user == nil || current_user.id != requested_user_id do       
+            conn      
+            |> put_flash(:error, "Nice try, friend. That's not a pagefor you.")      
+            |> redirect(to: AuctionWeb.Router.Helpers.item_path(conn, :index))      
+            |> halt()       
+        else      
+            conn            
+        end 
+    end
+
 end
