f6c339f0bc713c0e62226347d4cd7b388f4b742e
ksuna <ksuna@DESKTOP-SQMOH6T>
2018-12-04 18:57:51 -0600

    Fixed bid issues in bidcontroller

--- apps/auction/lib/auction/user.ex
+++ apps/auction/lib/auction/user.ex
@@ -6,7 +6,8 @@ defmodule Auction.User do
         field :username, :string    
         field :email_address, :string    
         field :password, :string, virtual: true                
-        field :hashed_password, :string    
+        field :hashed_password, :string 
+        has_many :bids, Auction.Bid   
         timestamps()  
     end 
 
--- apps/auction_web/lib/auction_web/controllers/bid_controller.ex
+++ apps/auction_web/lib/auction_web/controllers/bid_controller.ex
@@ -1,11 +1,12 @@
 defmodule AuctionWeb.BidController do
     use AuctionWeb, :controller
     plug :require_logged_in_user
+    alias AuctionWeb.Router.Helpers, as: Routes 
 
     def create(conn, %{"bid" => %{"amount" => amount}, "item_id" => item_id}) do
         user_id = conn.assigns.current_user.id
         case Auction.Bid.insert_bid(%{amount: amount, item_id: item_id, user_id: user_id}) do
-            {:ok, bid} -> redirect conn, to: AuctionWeb.Router.Helpers.item_path(conn, :show, bid.item_id)
+            {:ok, bid} -> redirect conn, to: Routes.item_path(conn, :show, bid.item_id)
             {:error, bid} -> 
                 item = Auction.get_item(item_id)
                 render conn, AuctionWeb.ItemView, "show.html", item: item, bid: bid
@@ -15,7 +16,7 @@ defmodule AuctionWeb.BidController do
     defp require_logged_in_user(%{assigns: %{current_user: nil}} = conn, _opts) do
         conn
         |> put_flash(:error, "Nice try, friend. You must be logged in to bid.")
-        |> redirect(to: Aunction.Route.Helpers.item_path(conn, :index))
+        |> redirect(to: Routes.item_path(conn, :index))
         |> halt()
     end
 
