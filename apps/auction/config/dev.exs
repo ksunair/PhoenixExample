126534f80ee13d1cacecdda57db456b92a2448e5
ksuna <ksuna@DESKTOP-SQMOH6T>
2018-12-08 21:14:25 -0600

    remove config files

---
+++ apps/auction/config/dev.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+config :auction, Auction.Repo,
+    database: "auction",
+    username: "postgres",
+    password: "password",
+    hostname: "localhost",
+    port: "5432"
---
+++ apps/auction/config/test.exs
@@ -0,0 +1,11 @@
+use Mix.Config
+
+config :auction, Auction.Repo,
+    database: "auction_test",
+    username: "postgres",
+    password: "password",
+    hostname: "localhost",
+    port: "5432",
+    pool: Ecto.Adapters.SQL.Sandbox
+
+config :logger, level: :info
